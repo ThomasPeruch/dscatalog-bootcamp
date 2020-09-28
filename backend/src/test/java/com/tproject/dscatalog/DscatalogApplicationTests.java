d039c6cd5511de148bf900df4f8227d5c407831b
ThomasPeruch <peruchthomas@gmail.com>
2020-09-28 17:39:21 -0300

    Project created

---
+++ backend/src/test/java/com/tproject/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tproject.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
